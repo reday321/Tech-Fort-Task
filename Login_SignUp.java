abc1473894b8f6243e735a30085a028c469a41d8
redayk321 <redayk37@gmail.com>
2025-11-19 17:18:05 +0600

    README.md added

--- Login_SignUp.java
+++ Login_SignUp.java
@@ -1,36 +1,28 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-
 import java.util.List;
 
-
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class Login_SignUp {
-    WebDriver driver;
 
+    WebDriver driver;
 
     @BeforeAll
     public void setup() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
     }
 
     @Test
     @Order(1)
+    @DisplayName("User can sign up successfully")
     public void SignUp() throws InterruptedException {
 
         driver.get("https://career.techforing.com/");
@@ -49,7 +41,6 @@ public class Login_SignUp {
         dateInput.sendKeys("10-8-1999");
         dateInput.sendKeys(Keys.ENTER);
 
-
         WebElement genderDropdown = wait.until(
                 ExpectedConditions.elementToBeClickable(
                         By.xpath("//label[text()='Gender']/following-sibling::div//div[contains(@class,'MuiSelect-select')]")
@@ -62,53 +53,36 @@ public class Login_SignUp {
         );
         maleOption.click();
 
-
         driver.findElement(By.id("email")).sendKeys("testRed@gmail.com");
         driver.findElement(By.id("password")).sendKeys("Reday221902026");
-
         driver.findElement(By.name("confirm_password")).sendKeys("Reday221902026");
 
-        WebElement captchaElement = driver.findElement(By.id("canv"));
-        String captchaText = CaptchaReader.readCaptcha(driver, captchaElement);
-        System.out.println("Detected CAPTCHA: " + captchaText);
-
-        try {
-
-            WebElement captchaInput = driver.findElement(By.cssSelector("input[placeholder='Enter the characters you see above']"));
-            captchaInput.clear();
-            captchaInput.sendKeys(captchaText);
-
-
-        } catch (Exception e) {
-            System.out.println("Placeholder selector failed, trying alternative selectors...");
-        }
-
+        //Waiting for manual CAPTCHA input
+        Thread.sleep(10000);
 
         WebElement verifyButton = driver.findElement(By.xpath("//button[text()='Verify Captcha']"));
         verifyButton.click();
 
         Thread.sleep(2000);
 
-        WebElement signUpButton = driver.findElement(By.xpath("//button[text()='Sign Up']"));
-
-        Thread.sleep(9000);
-
 
     }
 
     @Test
     @Order(2)
+    @DisplayName("User can login successfully")
     public void login() {
+
         driver.get("https://career.techforing.com/");
 
         List<WebElement> button = driver.findElements(By.className("MuiButton-root"));
-
         button.get(1).click();
-
         driver.findElement(By.id("email")).sendKeys("reday.gub@gmail.com");
         driver.findElement(By.id("password")).sendKeys("6CZyf@cT@RuL55w");
 
         driver.findElement(By.cssSelector("[type='submit']")).click();
-    }
-}
 
+
+    }
+
+    }
